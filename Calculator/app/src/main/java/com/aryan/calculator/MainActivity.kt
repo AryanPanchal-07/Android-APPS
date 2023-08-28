46fe127163e826ff927c7399e908705480ce8f34
Aryan Panchal <aryan.panchal.w@gmail.com>
2023-08-28 15:37:04 -0400

    1 to . button co,plete

--- Calculator/app/src/main/java/com/aryan/calculator/MainActivity.kt
+++ Calculator/app/src/main/java/com/aryan/calculator/MainActivity.kt
@@ -5,11 +5,18 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
+
 
 class MainActivity : AppCompatActivity() {
+
+    var startNumber: Boolean = true;
+    val acceptingDigit: Boolean = true;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val txtView: TextView = findViewById(R.id.numberDisplay)
+        txtView.text=""
     }
 
 
@@ -17,8 +24,48 @@ class MainActivity : AppCompatActivity() {
 
         //which button is pressed
         val button: Button = view as Button
-        val buttonText: String = button.text.toString();
+        val buttonText: String = button.text.toString()
 
         Log.i("button", "Button Clicked :$buttonText")
+
+        val textView: TextView = findViewById(R.id.numberDisplay)
+        var currentNumber = textView.text.toString()
+
+        //What Happens when button is pressed
+        when (buttonText) {
+            "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "." -> {
+                if(startNumber)
+                {
+                    if(buttonText.equals("."))
+                    {
+                        currentNumber = "0."
+                    }
+                    else {
+                        currentNumber = buttonText;
+                    }
+                    startNumber = false
+                }
+                else
+                {
+                    if(buttonText.equals(".") && currentNumber.contains("."))
+                    {
+
+                    }
+                    else {
+                        currentNumber += buttonText
+                    }
+                }
+
+            }
+        }
+        textView.text = currentNumber
+
+        when (buttonText) {
+            "C" -> {
+                currentNumber = ""
+                textView.text = currentNumber
+            }
+        }
+
     }
 }
